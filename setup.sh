d3eb92cae4de7670ee4982b5dbbe7b8e56d666d5
Kia-Jung Yang <kyang@lxplus766.cern.ch>
2023-06-25 15:10:31 +0200

    Delete # infront of sourcing the pbeast setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Setup tdaq-09-04-00"
-#source  /cvmfs/atlas.cern.ch/repo/sw/tdaq/tools/cmake_tdaq/bin/cm_setup.sh tdaq-09-04-00
+source  /cvmfs/atlas.cern.ch/repo/sw/tdaq/tools/cmake_tdaq/bin/cm_setup.sh tdaq-09-04-00
 
 echo "Set location of this base dir as global variable BASE_DIR"
 # Determine where this script is located
