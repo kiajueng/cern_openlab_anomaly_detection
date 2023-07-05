c0877940eddc5ff9c4b24a2d44cc3a9346c05b60
kyang <kyang@cern.ch>
2023-07-05 15:06:43 +0200

    Corrected Errors and now sorted into days

--- scripts/data_to_csv.sh
+++ scripts/data_to_csv.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
-partition=$1                                                                                                                                                                                   
-class=$2                                                                                                                                                                                   
-attribute=$3                                                                                                                                                                                  
-year=$4                                                                                                                                                                             
+partition=$1  
+class=$2
+attribute=$3                                
+year=$4               
 month=$5
-days=$6                                                                                                                                       
-regex=$7     #Variables of certain modules from which to read                                                                                                                            
+days=$6                                                               
+regex=$7     #Variables of certain modules from which to read    
 pattern="$(cut -d'.' -f1 <<<"$regex")" #  Pattern on which everything is split  
 
+echo "Month: ${month} Day: ${days} Year: {$year} Partition: ${partition} Class: ${class} Attribute: ${attribute} Regex: ${regex}"
 ############################################################# Read data from pbeast and clean it #########################################################################################################
-pbeast_read_repository -r /eos/atlas/atlascerngroupdisk/tdaq-opmon/pbeast -p "$partition" -c "$class" -a "$attribute" -s "${year}-${month}-${days} 0:00:00" -t "${year}-${month}-${days} 23:59:59" -O "$regex" > tmp.csv #Reads data from pbeast & save to tmp.csv
-
-sed -i '1,/'"$pattern"'/ { /'"$pattern"'/!d }' tmp.csv # Delete all lines until first module variable
-sed -i -e "/$pattern.*/s/://" tmp.csv # Deletes unnecessary : of each module it takes the values from                                                                                                     
+pbeast_read_repository -r /eos/atlas/atlascerngroupdisk/tdaq-opmon/pbeast -p "$partition" -c "$class" -a "$attribute" -s "${year}-${month}-${days} 00:00:00" -t "${year}-${month}-${days} 00:00:10" -O "$regex" > "${BASE_DIR}/tmp.csv" #Reads data from pbeast & save to "${BASE_DIR}/tmp.csv"
+sed -i '1,/'"$pattern"'/ { /'"$pattern"'/!d }' "${BASE_DIR}/tmp.csv" # Delete all lines until first module variable
+sed -i -e "/$pattern.*/s/://" "${BASE_DIR}/tmp.csv" # Deletes unnecessary : of each module it takes the values from
 
 ############################################################ Save each individual variable in its own .csv file ##########################################################################################
-
-mapfile -t lines < <(grep -n "$pattern.*" "tmp.csv" | cut -d: -f1) #Finds all lines with matching pattern
-
-last_line_number=$(wc -l < "tmp.csv") #append last line of file --> array has one value too much so only need to loop over len(array - 1)
+mapfile -t lines < <(grep -n "$pattern.*" "${BASE_DIR}/tmp.csv" | cut -d: -f1) #Finds all lines with matching pattern
+last_line_number=$(wc -l < "${BASE_DIR}/tmp.csv") #append last line of file --> array has one value too much so only need to loop over len(array - 1)
 last_line_number=$((last_line_number+1)) #Increment by one so last line is included 
 lines+=("$last_line_number")
 
 array_length=(${#lines[@]})
-
 if [[ ! -d "${BASE_DIR}/Data" ]]; then
     mkdir "${BASE_DIR}/Data"
 fi
@@ -45,12 +42,12 @@ for ((i=0; i<$(($array_length - 1)); i+=1)); do
     matched_line="${lines[$i]}"
     next_line="${lines[$((i+1))]}"
 
-    file_name="$(echo $(sed -n "${matched_line}p" tmp.csv) | sed 's/[./]/_/g').csv"  #Get line i and replace every "." and "/" with                                                                       
+    file_name="$(echo $(sed -n "${matched_line}p" "${BASE_DIR}/tmp.csv") | sed 's/[./]/_/g').csv"  #Get line i and replace every "." and "/" with
     # Do something with each matched line and the line that comes after it                    
-    awk -v start="$matched_line" -v end="$next_line" 'NR>=start && NR<end' "tmp.csv" > "${BASE_DIR}/Data/${year}/${month}/${days}/${file_name}" #Save everything between two linenumbers in new file, excluding second line number 
+    awk -v start="$matched_line" -v end="$next_line" 'NR>=start && NR<end' "${BASE_DIR}/tmp.csv" > "${BASE_DIR}/Data/${year}/${month}/${days}/${file_name}" #Save everything between two linenumbers in new file, excluding second line number 
     sed -i '1s/^/Date_Time,/' "${BASE_DIR}/Data/${year}/${month}/${days}/${file_name}"
     sed -i 's/    \[//g' "${BASE_DIR}/Data/${year}/${month}/${days}/${file_name}"
     sed -i 's/\] /,/g' "${BASE_DIR}/Data/${year}/${month}/${days}/${file_name}"
 done
 
-rm tmp.csv
+#rm "${BASE_DIR}/tmp.csv"
--- scripts/sort_to_days.sh
+++ scripts/sort_to_days.sh
@@ -9,21 +9,25 @@ start_month=$6
 end_month=$7
 start_day=$8
 end_day=$9
-regex=$10
+regex=${10}
 
 month=(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dez)
 
-if [ "$start_year" -ge "$end_year" ] && [ "$start_month" -eq "$end_month" ]; then
+if [ "$start_year" -ge "$end_year" ] && [ "$start_month" -gt "$end_month" ]; then
     echo "There has to be at least one month difference if the Year is the same and Start Year <= End Year"
-    return 1
+    exit 1
 fi
 
-while [ "$start_year" -ne "$end_year" ] || [ "$start_month" -ne "$end_month" ]; do
+while [ "$start_year" -ne "$end_year" ] || [ "$start_month" -le "$end_month" ]; do
 
     month_days=$(cal $(date +"${start_month} ${start_year}") | awk 'NF {DAYS = $NF}; END {print DAYS}')
 
-    while [ "$start_day" -le "month_days"]; do
-	./data_to_csv "$partition" "$class" "$attribute" "$start_year" "${month[$start_month]}" "$days" "$regex"	
+    while [ "$start_day" -le "$month_days" ]; do
+	source $BASE_DIR/scripts/data_to_csv.sh "$partition" "$class" "$attribute" "$start_year" "${month[$((start_month-1))]}" "$start_day" "$regex"
+	start_day=$((start_day+1))
+	if [ "$start_year" -eq "$end_year" ] && [ "$start_month" -eq "$end_month" ] && [ "$start_day" -gt "$end_day" ]; then
+	    exit 0
+	fi
     done
     
     if [ "$start_month" -eq 12 ]; then
