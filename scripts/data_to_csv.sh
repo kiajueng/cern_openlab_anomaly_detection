3016c8e06d9a9c4e451f4e20c7bfaa1c3bb06d5c
kyang <kyang@cern.ch>
2023-07-07 16:20:52 +0200

    deleted prints

--- scripts/data_to_csv.sh
+++ scripts/data_to_csv.sh
@@ -9,7 +9,7 @@ days=$6
 pattern=$7    #Pattern on which to split data to files  
 
 ############################################################# Read data from pbeast and pipe to tmp.csv ##################################################################################################
-pbeast_read_repository -r /eos/atlas/atlascerngroupdisk/tdaq-opmon/pbeast -p "$partition" -c "$class" -a "$attribute" -s "${year}-${month}-${days} 00:00:00" -t "${year}-${month}-${days} 00:01:00" -O "$regex" > "${BASE_DIR}/tmp.csv"
+pbeast_read_repository -r /eos/atlas/atlascerngroupdisk/tdaq-opmon/pbeast -p "$partition" -c "$class" -a "$attribute" -s "${year}-${month}-${days} 00:00:00" -t "${year}-${month}-${days} 23:59:59" -O "$regex" > "${BASE_DIR}/tmp.csv"
 ############################################################ Save each individual variable in its own .csv file ##########################################################################################
 if [[ ! -d "${BASE_DIR}/Data" ]]; then
     mkdir "${BASE_DIR}/Data"
@@ -27,7 +27,6 @@ if [[ ! -d "${BASE_DIR}/Data/${year}/${month}/${days}" ]]; then
     mkdir "${BASE_DIR}/Data/${year}/${month}/${days}"
 fi
 
-date
 python3 "${BASE_DIR}/scripts/data_cleaning.py" -d "$days" -m "$month" -y "$year" -p "$pattern"
-date
+
 rm "${BASE_DIR}/tmp.csv"
